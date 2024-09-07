90439e66fd8e9a6047480c54ad4eb45a478da550
Shivani Seth <shivaniseth_1@hotmail.com>
2024-09-07 10:36:29 +0000

    Craete Comment model

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,20 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+# Craete comment model.
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+        )
+    body = models.TextField()
+    approved = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
     
 
 
