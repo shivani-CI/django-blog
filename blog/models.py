86c49b760414f9adef6c06dd172c2549b646be81
Shivani Seth <shivaniseth_1@hotmail.com>
2024-09-09 14:46:25 +0000

    Add urls to blog

--- blog/models.py
+++ blog/models.py
@@ -21,8 +21,8 @@ class Post(models.Model):
         ordering = ["-created_on"]
         
                 
-        def __str__(self):
-            return f"{self.title}  |  written by {self.author}"
+    def __str__(self):
+        return f"{self.title}  |  written by {self.author}"
 
 
 # Craete comment model.
--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path("", views.PostList.as_view(), name='home'),
 ]
 
 
