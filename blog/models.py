f8c52fc8cdc6acf2bea3497b111ab918ff62e370
Shivani Seth <shivaniseth_1@hotmail.com>
2024-09-12 16:26:44 +0000

    Debug error in blog > model.py with indentation

--- blog/models.py
+++ blog/models.py
@@ -36,7 +36,7 @@ class Comment(models.Model):
     """
     Stores a single comment entry related to :model:`auth.User` and :model:`blog.Post`.
     """
-        post = models.ForeignKey(
+    post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments"
         )
     author = models.ForeignKey(
